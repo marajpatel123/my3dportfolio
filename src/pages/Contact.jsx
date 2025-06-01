0f6892a08e040926043e7f5717cabaeed9beca7e
Raj Patel <rajp739407@gmail.com>
2025-06-01 13:29:40 +0530

    url Update

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -34,7 +34,7 @@ const Contact = () => {
     
     try {
       const response = await axios.post(
-      "https://threed-portfolio-backend-de0c.onrender.com//contact",
+      "https://threed-portfolio-backend-e1q8.onrender.com/contact",
       formData,
       {
         headers: {
